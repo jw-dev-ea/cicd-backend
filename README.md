5d728c6bf2d7fb258828a0c16981cc6084032269
jw-dev-ea <jonathan.wengel.hsh@gmail.com>
2025-01-28 07:57:48 -0600

    v1

---
+++ README.md
@@ -0,0 +1,8 @@
+# CI/CD Backend
+
+
+### Build Docker image locally
+```bash
+docker build -t cicd-backend .
+docker run -p 3000:3000 cicd-backend
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Hardcoded version
+const version = '1.0.0';
+
+app.get('/version', (req, res) => {
+  res.json({ version });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
